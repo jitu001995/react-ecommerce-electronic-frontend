1bb13db3c07cb59b70ede87dddb16a676eb7b47c
jitendra <jitendrabe102@gmail.com>
2023-06-29 10:17:00 +0530

    modified app.js ffile

--- src/App.js
+++ src/App.js
@@ -9,13 +9,15 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1>Hello</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Ecommerce App
+          
         </a>
       </header>
     </div>
